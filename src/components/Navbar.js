cedc1bd51f65d042dd1fea2e6876270d171c8bed
Houda Boukhobza <houda.boukhobza@enetcom.u-sfax.tn>
2021-11-04 15:59:12 +0100

    ThirdCommit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -88,7 +88,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/houdaboukobza-lab/MyPortfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
